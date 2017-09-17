964cc97952f1f5818f66f3121c49169279b0c847
karn806 <nattakarn.kl@gmail.com>
2017-09-17 20:05:04 +0700

    got the board layout + each box index

---
+++ src/ui/CoinButton.java
@@ -0,0 +1,59 @@
+package ui;
+
+import entity.Board;
+import logic.BoardLogic;
+import logic.InitLogic;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by karn806 on 9/17/17.
+ */
+public class CoinButton extends JButton implements ActionListener {
+    ImageIcon B,W;
+
+    int x;
+    int y;
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    byte value=0;
+      /*
+      0:nothing
+      1: black
+      2: white
+      */
+
+    public CoinButton(){
+        B = new ImageIcon(this.getClass().getResource("b.png"));
+        W = new ImageIcon(this.getClass().getResource("w.png"));
+        this.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        System.out.println(x + " , " + y);
+        value++;
+        value%=3;
+        switch(value){
+            case 0:
+                setIcon(null);
+                break;
+            case 1:
+                setIcon(W);
+                break;
+            case 2:
+                setIcon(B);
+                break;
+        }
+    }
+}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,23 +1,74 @@
 package ui;
 
+import entity.Board;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import logic.BoardLogic;
+import logic.InitLogic;
 
-public class Main extends Application {
+import javax.swing.*;
+import java.awt.*;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("style.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+public class Main extends JFrame{
+
+    BoardLogic boardLogic = new BoardLogic();
+    InitLogic initLogic = new InitLogic();
+
+    Board board = initLogic.generateBoard();
+    int nCol = board.getNCOLS();
+    int nRow = board.getNROWS();
+    int dimension = nCol*nRow;
+
+    JPanel p = new JPanel();
+    CoinButton buttons[] = new CoinButton[dimension+1];
+
+
+    public static void main(String args[]){
+        new Main();
     }
 
+    public int newRow(int i, int x){
+        if (i!=1 && i%8==0){
+            x++;
+        }
+        return x;
+    }
 
-    public static void main(String[] args) {
-        launch(args);
+    public int newCol(int i, int y){
+        if (i==1){
+            y = 1;
+        }
+        else if (y!=0 && y%7==0){
+            y = 0;
+        } else {
+            y++;
+        }
+        return y;
+    }
+
+    public Main(){
+        super("Othello");
+        setSize(550,550);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        p.setLayout(new GridLayout(nRow,nCol));
+
+        int x = 0;
+        int y = 0;
+
+        for(int i = 1; i <= dimension; i++){
+            buttons[i] = new CoinButton();
+            buttons[i].setX(x);
+            buttons[i].setY(y);
+            p.add(buttons[i]);
+            x = newRow(i,x);
+            y = newCol(i,y);
+
+        }
+        add(p);
+
+        setVisible(true);
     }
 }
